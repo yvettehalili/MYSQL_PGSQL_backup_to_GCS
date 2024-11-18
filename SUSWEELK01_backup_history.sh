6dcc93c9a434d949a384973b76e94aaed4638a9c
yvettehalili <167964506+yvettehalili@users.noreply.github.com>
2024-11-18 19:59:34 +0800

    Update SUSWEELK01_backup_history.sh

--- SUSWEELK01_backup_history.sh
+++ SUSWEELK01_backup_history.sh
@@ -61,60 +61,55 @@ do
         SIZE=0
         FILENAMES=()
 
-        for DATE in "$TARGET_DATE" "$DATE_FORMATTED1" "$DATE_FORMATTED2"; do
-            # List all database directories under the server
-            for DB_FOLDER in $(gsutil ls "gs://$BUCKET/$BACKUP_PATH/" | grep '/$'); do
-                DB_FULL_PATH="${DB_FOLDER}FULL/"
-                DB_DIFF_PATH="${DB_FOLDER}DIFF/"
-                echo "Checking FULL directory: ${DB_FULL_PATH}"
-                echo "Checking DIFF directory: ${DB_DIFF_PATH}"
-                    
-                # Aggregate file lists from FULL and DIFF directories
-                FILES=$(gsutil ls "${DB_FULL_PATH}*${DATE}*.bak" 2>/dev/null)
-                FILES+=$(gsutil ls "${DB_DIFF_PATH}*${DATE}*.bak" 2>/dev/null)
-                    
-                if [[ -n "$FILES" ]]; then
-                    echo "Found backup files: $FILES"
-                        
-                    for FILE in $FILES; do
-                        fsize=$(gsutil du -s "$FILE" | awk '{print $1}')
-                        SIZE=$((SIZE + fsize))
-
-                        # Extract database name and filename from the full file path
-                        FILENAME=$(basename "$FILE")
-                        FILENAMES+=("$FILENAME")
-
-                        if [[ "$FILENAME" =~ ^${SERVER}_(.*)_(DIFF|FULL)_(.*)\.bak$ ]]; then
-                            DB_NAME="${BASH_REMATCH[1]}"
-                        fi
-
-                        echo "Backup details - Server: $SERVER, Database: $DB_NAME, Filename: $FILENAME, Filesize: $fsize, Path: $FILE"
-
-                        # Insert details into the backup log
-                        SQUERY="INSERT INTO backup_log (backup_date, server, size, filepath, last_update) 
-                                VALUES ('$TARGET_DATE','$SERVER',$fsize,'$FILE', NOW())
-                                ON DUPLICATE KEY UPDATE last_update=NOW(), size=$fsize;"
-                        echo "Inserting into backup_log: \"$SQUERY\""
-                        mysql -u"$DB_USER" -p"$DB_PASS" $DB_MAINTENANCE -e "$SQUERY"
-
-                        endcopy=$(date +"%Y-%m-%d %H:%M:%S")
-                        STATE="Completed"
-                        if [ "$SIZE" -eq 0 ]; then
-                            STATE="Error"
-                        fi
-
-                        # Insert each file's detail into the daily log with the backup status
-                        DQUERY="INSERT INTO daily_log (backup_date, server, \`database\`, size, state, last_update, fileName) 
-                                VALUES ('$TARGET_DATE', '$SERVER', '$DB_NAME', $fsize, '$STATE', '$endcopy', '$FILENAME');"
-                        echo "Inserting into daily_log: \"$DQUERY\""
-                        mysql -u"$DB_USER" -p"$DB_PASS" $DB_MAINTENANCE -e "$DQUERY"
-                    done
-                else
-                    echo "No backup files found for date: $DATE in ${DB_FULL_PATH} and ${DB_DIFF_PATH}"
-                fi
-            done
+        # List all database directories (including subdirectories) under the server
+        for DB_FOLDER in $(gsutil ls -d "gs://$BUCKET/$BACKUP_PATH/**" | grep '/$'); do
+            DB_FULL_PATH="${DB_FOLDER}FULL/"
+            DB_DIFF_PATH="${DB_FOLDER}DIFF/"
+            echo "Checking FULL directory: ${DB_FULL_PATH}"
+            echo "Checking DIFF directory: ${DB_DIFF_PATH}"
+                
+            # Aggregate file lists from FULL and DIFF directories
+            FILES=$(gsutil ls "${DB_FULL_PATH}*${DATE}*.bak" 2>/dev/null)
+            FILES+=$(gsutil ls "${DB_DIFF_PATH}*${DATE}*.bak" 2>/dev/null)
+                
             if [[ -n "$FILES" ]]; then
-                break
+                echo "Found backup files: $FILES"
+                    
+                for FILE in $FILES; do
+                    fsize=$(gsutil du -s "$FILE" | awk '{print $1}')
+                    SIZE=$((SIZE + fsize))
+
+                    # Extract database name and filename from the full file path
+                    FILENAME=$(basename "$FILE")
+                    FILENAMES+=("$FILENAME")
+
+                    if [[ "$FILENAME" =~ ^${SERVER}_(.*)_(DIFF|FULL)_(.*)\.bak$ ]]; then
+                        DB_NAME="${BASH_REMATCH[1]}"
+                    fi
+
+                    echo "Backup details - Server: $SERVER, Database: $DB_NAME, Filename: $FILENAME, Filesize: $fsize, Path: $FILE"
+
+                    # Insert details into the backup log
+                    SQUERY="INSERT INTO backup_log (backup_date, server, size, filepath, last_update) 
+                            VALUES ('$TARGET_DATE','$SERVER',$fsize,'$FILE', NOW())
+                            ON DUPLICATE KEY UPDATE last_update=NOW(), size=$fsize;"
+                    echo "Inserting into backup_log: \"$SQUERY\""
+                    mysql -u"$DB_USER" -p"$DB_PASS" $DB_MAINTENANCE -e "$SQUERY"
+
+                    endcopy=$(date +"%Y-%m-%d %H:%M:%S")
+                    STATE="Completed"
+                    if [ "$SIZE" -eq 0 ]; then
+                        STATE="Error"
+                    fi
+
+                    # Insert each file's detail into the daily log with the backup status
+                    DQUERY="INSERT INTO daily_log (backup_date, server, \`database\`, size, state, last_update, fileName) 
+                            VALUES ('$TARGET_DATE', '$SERVER', '$DB_NAME', $fsize, '$STATE', '$endcopy', '$FILENAME');"
+                    echo "Inserting into daily_log: \"$DQUERY\""
+                    mysql -u"$DB_USER" -p"$DB_PASS" $DB_MAINTENANCE -e "$DQUERY"
+                done
+            else
+                echo "No backup files found for date: $DATE in ${DB_FULL_PATH} and ${DB_DIFF_PATH}"
             fi
         done
     fi  # End of server name check
