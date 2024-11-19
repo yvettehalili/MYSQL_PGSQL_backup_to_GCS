d162827d2d6b3e22af341329256e8f92ccf08e6e
yvettehalili <167964506+yvettehalili@users.noreply.github.com>
2024-11-19 12:34:45 +0800

    Update db_bk_to_bucket_new.sh

--- db_bk_to_bucket_new.sh
+++ db_bk_to_bucket_new.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# Set environment variable to enable cleartext authentication plugin
+export LIBMYSQL_ENABLE_CLEARTEXT_PLUGIN=1
+
 TMP_PATH="/backup/dumps/"
 BACKUPS_PATH="/root/cloudstorage/Backups/Current/"
 BUCKET_PATH="/root/cloudstorage/"
@@ -35,7 +39,111 @@ for line in "${lines[@]}"; do
 
     DB_HOST=$HOST
     printf "${TIMESTAMP}: DUMPING SERVER: ${SERVER}\n"
-    
+
+    if [ "$SSL" != "y" ]; then
+        DB_LIST=$(mysql -u"$DB_USR" -p"$DB_PWD" --default-auth=mysql_native_password -h "$DB_HOST" -Bs -e "SHOW DATABASES")
+        if [ $? -ne 0 ]; then
+            printf "Error retrieving database list for server: ${SERVER}\n" >&2
+            continue
+        fi
+    else
+        DB_LIST=$(mysql -u"$DB_USR" -p"$DB_PWD" --default-auth=mysql_native_password -h "$DB_HOST" \
+                   --ssl-ca="${SSL_PATH}${SERVER}/server-ca.pem" \
+                   --ssl-cert="${SSL_PATH}${SERVER}/client-cert.pem" \
+                   --ssl-key="${SSL_PATH}${SERVER}/client-key.pem" -Bs -e "SHOW DATABASES")
+        if [ $? -ne 0 ]; then
+            printf "Error retrieving database list for server: ${SERVER}\n" >&2
+            continue
+        fi
+    fi
+
+    cd "${TMP_PATH}"
+    [ ! -d "${SERVER}" ] && mkdir -p $SERVER
+    cd $SERVER
+
+    for DB in $DB_LIST; do
+        if [[ "$DB" != "information_schema" ]] && [[ "$DB" != "performance_schema" ]] && [[ "$DB
+
+for DB in $DB_LIST; do
+    if [[ "$DB" != "information_schema" ]] && [[ "$DB" != "performance_schema" ]] && [[ "$DB" != "sys" ]] && [[ "$DB" != "mysql" ]]; then
+        printf "Dumping DB $DB\n"
+        if [ "$SSL" != "y" ]; then
+            mysqldump -u"$DB_USR" -p"$DB_PWD" --default-auth=mysql_native_password --set-gtid-purged=OFF --single-transaction --lock-tables=false --quick \
+                      --triggers --events --routines -h"$DB_HOST" $DB | gzip > "${CUR_DATE}_${DB}.sql.gz"
+        else
+            if [[ "$DB" == "db_hr_osticket_us" || "$DB" == "db_hr_osticket_ph" || "$DB" == "db_osticket_workday_global" || "$SERVER" == "isdba-cloudsql-us-we1-a-08" ]]; then
+                if [[ "$DAY" == "6" && "$DB" == "db_hr_osticket_ph" ]]; then
+                    mysqldump -u"$DB_USR" -p"$DB_PWD" --default-auth=mysql_native_password --set-gtid-purged=OFF \
+                              --ssl-ca="${SSL_PATH}${SERVER}/server-ca.pem" \
+                              --ssl-cert="${SSL_PATH}${SERVER}/client-cert.pem" \
+                              --ssl-key="${SSL_PATH}${SERVER}/client-key.pem" \
+                              --single-transaction --max_allowed_packet=2147483648 --hex-blob --net_buffer_length=4096 \
+                              --triggers --events --lock-tables=false --routines --quick -h"$DB_HOST" $DB | gzip > "${CUR_DATE}_${DB}.sql.gz"
+                else
+                    if [[ "$DB" == "db_hr_osticket_ph" ]]; then
+                        mysqldump -u"$DB_USR" -p"$DB_PWD" --default-auth=mysql_native_password --set-gtid-purged=OFF \
+                                  --ssl-ca="${SSL_PATH}${SERVER}/server-ca.pem" \
+                                  --ssl-cert="${SSL_PATH}${SERVER}/client-cert.pem" \
+                                  --ssl-key="${SSL_PATH}${SERVER}/client-key.pem" \
+                                  --single-transaction --max_allowed_packet=2147483648 --hex-blob --net_buffer_length=4096 \
+                                  --ignore-table=$DB.hr_file_chunk --lock-tables=false \
+                                  --triggers --events --routines --quick -h"$DB_HOST" $DB | gzip > "${CUR_DATE}_${DB}.sql.gz"
+                    else
+                        mysqldump -u"$DB_USR" -p"$DB_PWD" --default-auth=mysql_native_password --set-gtid-purged=OFF \
+                                  --ssl-ca="${SSL_PATH}${SERVER}/server-ca.pem" \
+                                  --ssl-cert="${SSL_PATH}${SERVER}/client-cert.pem" \
+                                  --ssl-key="${SSL_PATH}${SERVER}/client-key.pem" \
+                                  --single-transaction --max_allowed_packet=2147483648 --hex-blob --net_buffer_length=4096 \
+                                  --lock-tables=false --quick \
+                                  --triggers --events --routines -h"$DB_HOST" $DB | gzip > "${CUR_DATE}_${DB}.sql.gz"
+                    fi
+                fi
+            else
+                mysqldump -u"$DB_USR" -p"$DB_PWD" --default-auth=mysql_native_password --set-gtid-purged=OFF \
+                          --ssl-ca="${SSL_PATH}${SERVER}/server-ca.pem" \
+                          --ssl-cert="${SSL_PATH}${SERVER}/client-cert.pem" \
+                          --ssl-key="${SSL_PATH}${SERVER}/client-key.pem" \
+                          --single-transaction --max_allowed_packet=2147483648 --lock-tables=false --net_buffer_length=4096 \
+                          --triggers --events --routines --quick -h"$DB_HOST" $DB | gzip > "${CUR_DATE}_${DB}.sql.gz"
+            fi
+        fi
+        if [ $? -ne 0 ]; then
+            printf "Error dumping database: $DB on server: ${SERVER}\n" >&2
+       DEFAULT NULL,
+  `conservation_role`  varchar(100) DEFAULT NULL,
+  `GIS_AREA`           float        DEFAULT NULL,
+  `GIS_ML_START_DATE`  date         DEFAULT NULL,
+  `GIS_ML_END_DATE`    date         DEFAULT NULL,
+  PRIMARY KEY (`atu_pk`, `GIS_PRIMARY_SITE`),
+  KEY `idx_atu_pk` (`atu_pk`),
+  KEY `idx_primary_site` (`GIS_PRIMARY_SITE`)  ) ENGINE=InnoDB "
+
+# This Environment Variable Enables Cleartext Password Plugin
+export LIBMYSQL_ENABLE_CLEARTEXT_PLUGIN=1
+
+SERVERS_LIST="/backup/configs/servers_list.csv"
+readarray -t lines < "${SERVERS_LIST}"
+
+printf "================================== ${CUR_DATE} =============================================\n"
+
+for line in "${lines[@]}"; do
+    column_values=$(echo $line | tr "," "\n")
+    I=0
+    for value in $column_values
+    do
+        if [ $I == 0 ]; then
+            SERVER=$value
+        elif [ $I == 1 ]; then
+            HOST=$value
+        elif [ $I == 2 ]; then
+            SSL=$value
+        fi
+        I=$((I+1))
+    done
+
+    DB_HOST=$HOST
+    printf "${TIMESTAMP}: DUMPING SERVER: ${SERVER}\n"
+
     if [ "$SSL" != "y" ]; then
         DB_LIST=$(mysql -u"$DB_USR" -p"$DB_PWD" --default-auth=mysql_native_password -h "$DB_HOST" -Bs -e "SHOW DATABASES")
         if [ $? -ne 0 ]; then
