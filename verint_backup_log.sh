8790ebc7239223b667b92594ed31f24f15c41536
yvettehalili <167964506+yvettehalili@users.noreply.github.com>
2024-11-22 18:32:47 +0800

    Update verint_backup_log.sh

--- verint_backup_log.sh
+++ verint_backup_log.sh
@@ -15,10 +15,10 @@ DB_MAINTENANCE=ti_db_inventory
 STORAGE=/root/cloudstorage
 BUCKET=ticxwfo-dbbackup
 
-# Date for Checking Backups
-TARGET_DATE="2024-11-17"
-DATE_FORMATTED1=$(date -d "$TARGET_DATE" +"%Y%m%d")
-DATE_FORMATTED2=$(date -d "$TARGET_DATE" +"%d-%m-%Y")
+# Date Calculation
+CURRENT_SATURDAY=$(date +"%Y-%m-%d")
+START_DATE=$(date -d "$CURRENT_SATURDAY -6 days" +"%Y-%m-%d")
+END_DATE=$(date -d "$CURRENT_SATURDAY -1 days" +"%Y-%m-%d")
 
 # SQL Query to Fetch Server Details
 query="SELECT name, ip, user, pwd, os, frequency, save_path, location, type FROM ti_db_inventory.servers WHERE active = 1 AND project = 'ti-verint152prod' ORDER BY location, type, os;"
@@ -45,7 +45,7 @@ echo "Mounting bucket for project ti-verint152prod"
 gcsfuse --key-file=/root/jsonfiles/ti-verint152prod-4ba9008f4ef8.json $BUCKET $STORAGE || { echo "Error mounting gcsfuse for ti-verint152prod"; exit 1; }
 
 echo "============================================================================================================"
-echo "CHECKING DATE: $TARGET_DATE"
+echo "CHECKING DATE RANGE: $START_DATE to $END_DATE"
 echo "============================================================================================================"
 
 # Fetch and iterate over server details from the database
@@ -57,7 +57,7 @@ do
     echo "============================================================================================================"
     echo "SERVER: $SERVER - $SERVERIP - $OS - $TYPE - $SAVE_PATH - $LOCATION"
     echo "============================================================================================================"
-    echo "Checking backups for SERVER: $SERVER on DATE: $TARGET_DATE"
+    echo "Checking backups for SERVER: $SERVER from DATE: $START_DATE to $END_DATE"
 
     BACKUP_PATH="V152_Backups/$SERVER"
     echo "Backup path being checked: $BACKUP_PATH"
@@ -67,62 +67,68 @@ do
 
     # Handle MSSQL separately due to different backup structure
     if [[ "$TYPE" == "MSSQL" ]]; then
-        for DATE in "$TARGET_DATE" "$DATE_FORMATTED1" "$DATE_FORMATTED2"; do
-            # List all database directories under the server
-            for DB_FOLDER in $(gsutil ls "gs://$BUCKET/$BACKUP_PATH/" | grep '/$'); do
-                DB_FULL_PATH="${DB_FOLDER}FULL/"
-                DB_DIFF_PATH="${DB_FOLDER}DIFF/"
-                echo "Checking FULL directory: ${DB_FULL_PATH}"
-                echo "Checking DIFF directory: ${DB_DIFF_PATH}"
+        for DAY in {0..6}; do
+            TARGET_DATE=$(date -d "$START_DATE +$DAY days" +"%Y-%m-%d")
+            DATE_FORMATTED1=$(date -d "$TARGET_DATE" +"%Y%m%d")
+            DATE_FORMATTED2=$(date -d "$TARGET_DATE" +"%d-%m-%Y")
+            
+            for DATE in "$TARGET_DATE" "$DATE_FORMATTED1" "$DATE_FORMATTED2"; do
+                # List all database directories under the server
+                for DB_FOLDER in $(gsutil ls "gs://$BUCKET/$BACKUP_PATH/" | grep '/$'); do
+                    DB_FULL_PATH="${DB_FOLDER}FULL/"
+                    DB_DIFF_PATH="${DB_FOLDER}DIFF/"
+                    echo "Checking FULL directory: ${DB_FULL_PATH}"
+                    echo "Checking DIFF directory: ${DB_DIFF_PATH}"
 
-                # Aggregate file lists from FULL and DIFF directories
-                FILES=$(gsutil ls "${DB_FULL_PATH}*${DATE}*.bak" 2>/dev/null)
-                FILES+=$(gsutil ls "${DB_DIFF_PATH}*${DATE}*.bak" 2>/dev/null)
+                    # Aggregate file lists from FULL and DIFF directories
+                    FILES=$(gsutil ls "${DB_FULL_PATH}*${DATE}*.bak" 2>/dev/null)
+                    FILES+=$(gsutil ls "${DB_DIFF_PATH}*${DATE}*.bak" 2>/dev/null)
 
+                    if [[ -n "$FILES" ]]; then
+                        echo "Found backup files: $FILES"
+
+                        for FILE in $FILES; do
+                            fsize=$(gsutil du -s "$FILE" | awk '{print $1}')
+                            SIZE=$((SIZE + fsize))
+
+                            # Extract database name and filename from the full file path
+                            FILENAME=$(basename "$FILE")
+                            FILENAMES+=("$FILENAME")
+
+                            if [[ "$FILENAME" =~ ^${SERVER}_(.*)_(DIFF|FULL)_(.*)\.bak$ ]]; then
+                                DB_NAME="${BASH_REMATCH[1]}"
+                            fi
+
+                            echo "Backup details - Server: $SERVER, Database: $DB_NAME, Filename: $FILENAME, Filesize: $fsize, Path: $FILE"
+
+                            # Insert details into the backup log
+                            SQUERY="INSERT INTO backup_log (backup_date, server, size, filepath, last_update) 
+                                    VALUES ('$TARGET_DATE','$SERVER',$fsize,'$FILE', NOW())
+                                    ON DUPLICATE KEY UPDATE last_update=NOW(), size=$fsize;"
+                            echo "Inserting into backup_log: \"$SQUERY\""
+                            mysql -u"$DB_USER" -p"$DB_PASS" $DB_MAINTENANCE -e "$SQUERY"
+
+                            endcopy=$(date +"%Y-%m-%d %H:%M:%S")
+                            STATE="Completed"
+
+                            if [ "$SIZE" -eq 0 ]; then
+                                STATE="Error"
+                            fi
+
+                            # Insert each file's detail into the daily log with the backup status
+                            DQUERY="INSERT INTO daily_log (backup_date, server, \`database\`, size, state, last_update, fileName) 
+                                    VALUES ('$TARGET_DATE', '$SERVER', '$DB_NAME', $fsize, '$STATE', '$endcopy', '$FILENAME');"
+                            echo "Inserting into daily_log: \"$DQUERY\""
+                            mysql -u"$DB_USER" -p"$DB_PASS" $DB_MAINTENANCE -e "$DQUERY"
+                        done
+                    else
+                        echo "No backup files found for date: $DATE in ${DB_FULL_PATH} and ${DB_DIFF_PATH}"
+                    fi
+                done
                 if [[ -n "$FILES" ]]; then
-                    echo "Found backup files: $FILES"
-
-                    for FILE in $FILES; do
-                        fsize=$(gsutil du -s "$FILE" | awk '{print $1}')
-                        SIZE=$((SIZE + fsize))
-
-                        # Extract database name and filename from the full file path
-                        FILENAME=$(basename "$FILE")
-                        FILENAMES+=("$FILENAME")
-
-                        if [[ "$FILENAME" =~ ^${SERVER}_(.*)_(DIFF|FULL)_(.*)\.bak$ ]]; then
-                            DB_NAME="${BASH_REMATCH[1]}"
-                        fi
-
-                        echo "Backup details - Server: $SERVER, Database: $DB_NAME, Filename: $FILENAME, Filesize: $fsize, Path: $FILE"
-
-                        # Insert details into the backup log
-                        SQUERY="INSERT INTO backup_log (backup_date, server, size, filepath, last_update) 
-                                VALUES ('$TARGET_DATE','$SERVER',$fsize,'$FILE', NOW())
-                                ON DUPLICATE KEY UPDATE last_update=NOW(), size=$fsize;"
-                        echo "Inserting into backup_log: \"$SQUERY\""
-                        mysql -u"$DB_USER" -p"$DB_PASS" $DB_MAINTENANCE -e "$SQUERY"
-
-                        endcopy=$(date +"%Y-%m-%d %H:%M:%S")
-                        STATE="Completed"
-
-                        if [ "$SIZE" -eq 0 ]; then
-                            STATE="Error"
-                        fi
-
-                        # Insert each file's detail into the daily log with the backup status
-                        DQUERY="INSERT INTO daily_log (backup_date, server, \`database\`, size, state, last_update, fileName) 
-                                VALUES ('$TARGET_DATE', '$SERVER', '$DB_NAME', $fsize, '$STATE', '$endcopy', '$FILENAME');"
-                        echo "Inserting into daily_log: \"$DQUERY\""
-                        mysql -u"$DB_USER" -p"$DB_PASS" $DB_MAINTENANCE -e "$DQUERY"
-                    done
-                else
-                    echo "No backup files found for date: $DATE in ${DB_FULL_PATH} and ${DB_DIFF_PATH}"
+                    break
                 fi
             done
-            if [[ -n "$FILES" ]]; then
-                break
-            fi
         done
     else
         echo "Skipping non-MSSQL server: $SERVER"
